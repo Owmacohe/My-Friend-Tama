462c87f8a5e569e77e1ed3b255df176d44f1c395
rabbit-isdead <70164577+m-hops@users.noreply.github.com>
2022-11-23 15:06:31 -0500

    shader bug fix

--- Assets/Scripts/Environment/MaterialSwitcherScript.cs
+++ Assets/Scripts/Environment/MaterialSwitcherScript.cs
@@ -5,44 +5,34 @@ using UnityEngine;
 public class MaterialSwitcherScript : MonoBehaviour
 {
     [SerializeField] GameObject arcadeCab;
-    public bool poweredOn = false;
    
     Material[] materials;
     Material screenMaterial;
-    float timePassed = 0f;
+    PlayerObjectDetection playerObjectDetection;
+
 
     private void Start()
     {
-        ///Keeps all screens black at start up until player collects coin
-        materials = arcadeCab.GetComponent<Renderer>().materials;
-        screenMaterial = materials[1];
-        screenMaterial.SetColor("_EmissionColor", Color.black);
+        initialSceenSetup();
+        playerObjectDetection = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerObjectDetection>();
+    
     }
 
     void Update()
     {
-        ///For some reason, the game freezes up if poweredOn is true
-        ///Leaving off for now until I can troubleshoot later
-        while (poweredOn)
-        {
-            timePassed += Time.deltaTime;
+       poweredOn();
+    }
 
-            if (timePassed < 3f)
-            {
-                screenMaterial.SetColor("_EmissionColor", Color.yellow);
-            } 
-            else if (timePassed > 3f && timePassed < 6f)
-            {
-                screenMaterial.SetColor("_EmissionColor", Color.red);
-            }
-            else if (timePassed > 6f && timePassed < 9f)
-            {
-                screenMaterial.SetColor("_EmissionColor", Color.blue);
-            }
-            else if (timePassed > 12f)
-            {
-                timePassed = 0f;
-            }
-        }
+    void initialSceenSetup()
+    {
+        ///Keeps all screens black at start up until player collects coin
+        materials = arcadeCab.GetComponent<MeshRenderer>().materials;
+        screenMaterial = materials[1];
+        screenMaterial.SetColor("_EmissionColor", Color.black);
+    }
+
+    void poweredOn()
+    {
+        screenMaterial.SetColor("_EmissionColor", Color.blue);
     }
 }
