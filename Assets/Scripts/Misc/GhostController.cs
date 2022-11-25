d3aaf0837bb884f01cc66b619286dfad647ec56e
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-11-24 22:16:25 -0500

    Added basic ghosts

---
+++ Assets/Scripts/Misc/GhostController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GhostController : MonoBehaviour
+{
+    [SerializeField] float shuffleSpeed = 2;
+    
+    Transform mainCam;
+    Rigidbody rb;
+
+    void Start()
+    {
+        mainCam = Camera.main.transform;
+        rb = GetComponent<Rigidbody>();
+        
+        GetComponent<BoxCollider>().center = Vector3.up * Random.Range(0.25f, 1.5f);
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(mainCam);
+        transform.localEulerAngles = Vector3.up * transform.localEulerAngles.y;
+        
+        rb.AddForce(new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)) * shuffleSpeed);
+
+        if (Time.time % 4 == 0)
+        {
+            rb.AddForce(Vector3.up * (Random.Range(0f, 4f) * shuffleSpeed));
+        }
+    }
+}
