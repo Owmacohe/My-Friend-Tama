da1d20d16beaa89fe136a2bab5703864c43584af
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-12-02 19:05:33 -0500

    Started work on chat system

---
+++ Assets/Scripts/Chat/ChatController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChatController : MonoBehaviour
+{
+    [SerializeField] GameObject comment;
+    [SerializeField] int maxComments = 6;
+    [SerializeField] float averageCommentSpeed = 2;
+    [SerializeField] Color[] nameColours;
+
+    Queue<GameObject> commentQueue = new Queue<GameObject>();
+
+    void Start()
+    {
+        Invoke(nameof(AddComment), averageCommentSpeed);
+    }
+
+    void AddComment()
+    {
+        AddComment(ChatGenerator.RandomName(), ChatGenerator.RandomMessage());
+        
+        Invoke(
+            nameof(AddComment), 
+            Random.Range(
+                -averageCommentSpeed * 0.8f,
+                averageCommentSpeed * 0.8f
+            ) + averageCommentSpeed
+        );
+    }
+
+    void AddComment(string name, string message)
+    {
+        GameObject temp = Instantiate(comment, transform);
+
+        TMP_Text[] fields = temp.GetComponentsInChildren<TMP_Text>();
+        fields[0].text = name;
+        fields[0].color = nameColours[Random.Range(0, nameColours.Length)];
+        fields[1].text = message;
+
+        if (commentQueue.Count == maxComments)
+        {
+            Destroy(commentQueue.Dequeue());
+        }
+        
+        commentQueue.Enqueue(temp);
+    }
+}
---
+++ Assets/Scripts/Chat/ChatGenerator.cs
@@ -0,0 +1,508 @@
+﻿using UnityEngine;
+
+public class ChatGenerator
+{
+    static readonly string[] firstNames = {
+        "Aaliyah",
+        "Aaron",
+        "Abby",
+        "Abigail",
+        "Abraham",
+        "Adam",
+        "Addison",
+        "Adrian",
+        "Adriana",
+        "Adrianna",
+        "Aidan",
+        "Aiden",
+        "Alan",
+        "Alana",
+        "Alejandro",
+        "Alex",
+        "Alexa",
+        "Alexander",
+        "Alexandra",
+        "Alexandria",
+        "Alexia",
+        "Alexis",
+        "Alicia",
+        "Allison",
+        "Alondra",
+        "Alyssa",
+        "Amanda",
+        "Amber",
+        "Amelia",
+        "Amy",
+        "Ana",
+        "Andrea",
+        "Andres",
+        "Andrew",
+        "Angel",
+        "Angela",
+        "Angelica",
+        "Angelina",
+        "Anna",
+        "Anthony",
+        "Antonio",
+        "Ariana",
+        "Arianna",
+        "Ashley",
+        "Ashlyn",
+        "Ashton",
+        "Aubrey",
+        "Audrey",
+        "Austin",
+        "Autumn",
+        "Ava",
+        "Avery",
+        "Ayden",
+        "Bailey",
+        "Benjamin",
+        "Bianca",
+        "Blake",
+        "Braden",
+        "Bradley",
+        "Brady",
+        "Brandon",
+        "Brayden",
+        "Breanna",
+        "Brendan",
+        "Brian",
+        "Briana",
+        "Brianna",
+        "Brittany",
+        "Brody",
+        "Brooke",
+        "Brooklyn",
+        "Bryan",
+        "Bryce",
+        "Bryson",
+        "Caden",
+        "Caitlin",
+        "Caitlyn",
+        "Caleb",
+        "Cameron",
+        "Camila",
+        "Carlos",
+        "Caroline",
+        "Carson",
+        "Carter",
+        "Cassandra",
+        "Cassidy",
+        "Catherine",
+        "Cesar",
+        "Charles",
+        "Charlotte",
+        "Chase",
+        "Chelsea",
+        "Cheyenne",
+        "Chloe",
+        "Christian",
+        "Christina",
+        "Christopher",
+        "Claire",
+        "Cody",
+        "Colby",
+        "Cole",
+        "Colin",
+        "Collin",
+        "Colton",
+        "Conner",
+        "Connor",
+        "Cooper",
+        "Courtney",
+        "Cristian",
+        "Crystal",
+        "Daisy",
+        "Dakota",
+        "Dalton",
+        "Damian",
+        "Daniel",
+        "Daniela",
+        "Danielle",
+        "David",
+        "Delaney",
+        "Derek",
+        "Destiny",
+        "Devin",
+        "Devon",
+        "Diana",
+        "Diego",
+        "Dominic",
+        "Donovan",
+        "Dylan",
+        "Edgar",
+        "Eduardo",
+        "Edward",
+        "Edwin",
+        "Eli",
+        "Elias",
+        "Elijah",
+        "Elizabeth",
+        "Ella",
+        "Ellie",
+        "Emily",
+        "Emma",
+        "Emmanuel",
+        "Eric",
+        "Erica",
+        "Erick",
+        "Erik",
+        "Erin",
+        "Ethan",
+        "Eva",
+        "Evan",
+        "Evelyn",
+        "Faith",
+        "Fernando",
+        "Francisco",
+        "Gabriel",
+        "Gabriela",
+        "Gabriella",
+        "Gabrielle",
+        "Gage",
+        "Garrett",
+        "Gavin",
+        "Genesis",
+        "George",
+        "Gianna",
+        "Giovanni",
+        "Giselle",
+        "Grace",
+        "Gracie",
+        "Grant",
+        "Gregory",
+        "Hailey",
+        "Haley",
+        "Hannah",
+        "Hayden",
+        "Hector",
+        "Henry",
+        "Hope",
+        "Hunter",
+        "Ian",
+        "Isaac",
+        "Isabel",
+        "Isabella",
+        "Isabelle",
+        "Isaiah",
+        "Ivan",
+        "Jack",
+        "Jackson",
+        "Jacob",
+        "Jacqueline",
+        "Jada",
+        "Jade",
+        "Jaden",
+        "Jake",
+        "Jalen",
+        "James",
+        "Jared",
+        "Jasmin",
+        "Jasmine",
+        "Jason",
+        "Javier",
+        "Jayden",
+        "Jayla",
+        "Jazmin",
+        "Jeffrey",
+        "Jenna",
+        "Jennifer",
+        "Jeremiah",
+        "Jeremy",
+        "Jesse",
+        "Jessica",
+        "Jesus",
+        "Jillian",
+        "Jocelyn",
+        "Joel",
+        "John",
+        "Johnathan",
+        "Jonah",
+        "Jonathan",
+        "Jordan",
+        "Jordyn",
+        "Jorge",
+        "Jose",
+        "Joseph",
+        "Joshua",
+        "Josiah",
+        "Juan",
+        "Julia",
+        "Julian",
+        "Juliana",
+        "Justin",
+        "Kaden",
+        "Kaitlyn",
+        "Kaleb",
+        "Karen",
+        "Karina",
+        "Kate",
+        "Katelyn",
+        "Katherine",
+        "Kathryn",
+        "Katie",
+        "Kayla",
+        "Kaylee",
+        "Kelly",
+        "Kelsey",
+        "Kendall",
+        "Kennedy",
+        "Kenneth",
+        "Kevin",
+        "Kiara",
+        "Kimberly",
+        "Kyle",
+        "Kylee",
+        "Kylie",
+        "Landon",
+        "Laura",
+        "Lauren",
+        "Layla",
+        "Leah",
+        "Leonardo",
+        "Leslie",
+        "Levi",
+        "Liam",
+        "Liliana",
+        "Lillian",
+        "Lilly",
+        "Lily",
+        "Lindsey",
+        "Logan",
+        "Lucas",
+        "Lucy",
+        "Luis",
+        "Luke",
+        "Lydia",
+        "Mackenzie",
+        "Madeline",
+        "Madelyn",
+        "Madison",
+        "Makayla",
+        "Makenzie",
+        "Malachi",
+        "Manuel",
+        "Marco",
+        "Marcus",
+        "Margaret",
+        "Maria",
+        "Mariah",
+        "Mario",
+        "Marissa",
+        "Mark",
+        "Martin",
+        "Mary",
+        "Mason",
+        "Matthew",
+        "Max",
+        "Maxwell",
+        "Maya",
+        "Mckenzie",
+        "Megan",
+        "Melanie",
+        "Melissa",
+        "Mia",
+        "Micah",
+        "Michael",
+        "Michelle",
+        "Miguel",
+        "Mikayla",
+        "Miranda",
+        "Molly",
+        "Morgan",
+        "Mya",
+        "Naomi",
+        "Natalia",
+        "Natalie",
+        "Nathan",
+        "Nathaniel",
+        "Nevaeh",
+        "Nicholas",
+        "Nicolas",
+        "Nicole",
+        "Noah",
+        "Nolan",
+        "Oliver",
+        "Olivia",
+        "Omar",
+        "Oscar",
+        "Owen",
+        "Paige",
+        "Parker",
+        "Patrick",
+        "Paul",
+        "Payton",
+        "Peter",
+        "Peyton",
+        "Preston",
+        "Rachel",
+        "Raymond",
+        "Reagan",
+        "Rebecca",
+        "Ricardo",
+        "Richard",
+        "Riley",
+        "Robert",
+        "Ruby",
+        "Ryan",
+        "Rylee",
+        "Sabrina",
+        "Sadie",
+        "Samantha",
+        "Samuel",
+        "Sara",
+        "Sarah",
+        "Savannah",
+        "Sean",
+        "Sebastian",
+        "Serenity",
+        "Sergio",
+        "Seth",
+        "Shane",
+        "Shawn",
+        "Shelby",
+        "Sierra",
+        "Skylar",
+        "Sofia",
+        "Sophia",
+        "Sophie",
+        "Spencer",
+        "Stephanie",
+        "Stephen",
+        "Steven",
+        "Summer",
+        "Sydney",
+        "Tanner",
+        "Taylor",
+        "Thomas",
+        "Tiffany",
+        "Timothy",
+        "Travis",
+        "Trenton",
+        "Trevor",
+        "Trinity",
+        "Tristan",
+        "Tyler",
+        "Valeria",
+        "Valerie",
+        "Vanessa",
+        "Veronica",
+        "Victor",
+        "Victoria",
+        "Vincent",
+        "Wesley",
+        "William",
+        "Wyatt",
+        "Xavier",
+        "Zachary",
+        "Zoe",
+        "Zoey"
+    };
+
+    static readonly string[] messages =
+    {
+        "Where are you?",
+        "This is boring",
+        "What are you doing?",
+        "Did you see that?",
+        "You're not supposed to be here!",
+        "Where can I find this place?",
+        "Ew gross",
+        "This is so spooky",
+        "How did you get in?",
+        "Pog",
+        "Hey, what's up",
+        "I finally caught you live!",
+        "Wow!",
+        "I've been here",
+        "Is this place abandoned?"
+    };
+
+    static string RandomFirstName()
+    {
+        string firstName = "";
+
+        string temp = firstNames[Random.Range(0, firstNames.Length)].ToLower();
+
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (temp[i].Equals('o') && Random.Range(0, 5) == 0)
+            {
+                firstName += '0';
+            }
+            else if (temp[i].Equals('i') && Random.Range(0, 5) == 0)
+            {
+                firstName += '1';
+            }
+            else if (temp[i].Equals('e') && Random.Range(0, 5) == 0)
+            {
+                firstName += '3';
+            }
+            else if (temp[i].Equals('s') && Random.Range(0, 5) == 0)
+            {
+                firstName += '5';
+            }
+            else if (temp[i].Equals('b') && Random.Range(0, 5) == 0)
+            {
+                firstName += '8';
+            }
+            else if (temp[i].Equals('g') && Random.Range(0, 5) == 0)
+            {
+                firstName += '9';
+            }
+            else
+            {
+                firstName += Random.Range(0, 7) == 0 ? temp[i].ToString().ToUpper() : temp[i];   
+            }
+
+            if (Random.Range(0, 10) == 0)
+            {
+                string specialCharacters = "?<>_+=|~.-";
+                firstName += specialCharacters[Random.Range(0, specialCharacters.Length)];
+            }
+        }
+
+        return firstName;
+    }
+
+    public static string RandomName()
+    {
+        string name = RandomFirstName();
+
+        if (Random.Range(0, 10) == 0)
+        {
+            string temp = RandomFirstName();
+            name += "_" + temp.Substring(0, Random.Range(0, temp.Length));
+        }
+
+        if (Random.Range(0, 2) == 0)
+        {
+            for (int j = 0; j < Random.Range(1, 3); j++)
+            {
+                name += Random.Range(0, 9);
+            }
+        }
+        else if (Random.Range(0, 5) == 0)
+        {
+            name = "xX_" + name + "_Xx";
+        }
+        
+        return name;
+    }
+
+    public static string RandomMessage()
+    {
+        if (Random.Range(0, 10) == 0)
+        {
+            return "[DONATED $" + Random.Range(5, 100) + "]";
+        }
+        
+        if (Random.Range(0, 10) == 0)
+        {
+            return "[SUBSCRIBED]";
+        }
+        
+        return messages[Random.Range(0, messages.Length)];
+    }
+}
