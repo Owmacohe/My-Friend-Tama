7e5859a62a37d9ce6f9e9d00eb6477068abe2c23
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-12-06 12:55:34 -0500

    ALL THE FIXES ARE DONE NOW

--- Assets/Scripts/Sound/TutorialSoundsController.cs
+++ Assets/Scripts/Sound/TutorialSoundsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -6,17 +7,30 @@ public class TutorialSoundsController : MonoBehaviour
 {
     [SerializeField] AudioClip[] mallTutorials;
     [SerializeField] AudioClip[] streamerTutorials;
+    [SerializeField] GameObject seeTamaVolume, seeFoodCourtVolume, seeArcadeVolume, seeBathroomVolume;
 
-    AudioSource[] PAs;
+    List<AudioSource> PAs = new List<AudioSource>();
     AudioSource streamerSource;
     
     [HideInInspector] public int tutorialProgress;
+    int streamerProgress;
     [HideInInspector] public bool isPlaying;
 
     void Start()
     {
         streamerSource = GetComponent<AudioSource>();
-        PAs = GetComponentsInChildren<AudioSource>();
+        
+        AudioSource[] temp = GetComponentsInChildren<AudioSource>();
+
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (i > 0)
+            {
+                PAs.Add(temp[i]);
+            }
+        }
+        
+        PlayStreamerTutorial(0);
     }
 
     void Update()
@@ -33,7 +47,7 @@ public class TutorialSoundsController : MonoBehaviour
 
     public void PlayMallTutorial(int tut)
     {
-        if (tut == tutorialProgress && tut < PAs.Length)
+        if (tut == tutorialProgress && tut < PAs.Count)
         {
             foreach (AudioSource i in PAs)
             {
@@ -50,9 +64,41 @@ public class TutorialSoundsController : MonoBehaviour
         }
     }
 
-    public void PlayStreamerTutorial(int tut)
+    public void PlayNextStreamerTutorial()
     {
-        streamerSource.clip = streamerTutorials[tut];
-        streamerSource.Play();
+        PlayStreamerTutorial(streamerProgress);
+    }
+
+    public void PlayStreamerTutorial(GameObject volume)
+    {
+        if (volume.Equals(seeTamaVolume))
+        {
+            PlayStreamerTutorial(1);
+        }
+        else if (volume.Equals(seeFoodCourtVolume))
+        {
+            PlayStreamerTutorial(3);
+        }
+        else if (volume.Equals(seeArcadeVolume))
+        {
+            PlayStreamerTutorial(5);
+        }
+        else if (volume.Equals(seeBathroomVolume))
+        {
+            PlayStreamerTutorial(7);
+        }
+    }
+
+    void PlayStreamerTutorial(int tut)
+    {
+        if (tut == streamerProgress)
+        {
+            print("test");
+            
+            streamerSource.clip = streamerTutorials[tut];
+            streamerSource.Play();
+
+            streamerProgress++;   
+        }
     }
 }
