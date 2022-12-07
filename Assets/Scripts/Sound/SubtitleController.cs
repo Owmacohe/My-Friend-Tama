d9b650bd3c569171f6ffab3fc185c48221dcbd6c
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-12-07 09:02:35 -0500

    Added subtitle colours

--- Assets/Scripts/Sound/SubtitleController.cs
+++ Assets/Scripts/Sound/SubtitleController.cs
@@ -7,17 +7,24 @@ public class SubtitleController : MonoBehaviour
 {
     [SerializeField] Image background;
     [SerializeField] TMP_Text text;
+    [SerializeField] Color tutorialColour, streamerColour;
 
     [HideInInspector] public bool isPopulated;
     [HideInInspector] public bool isTutorial;
 
     public void Populate(string subtitle, bool tutorial)
     {
+        Color temp = tutorial ? tutorialColour : streamerColour;
+        
         background.enabled = true;
+        background.color = new Color(temp.r, temp.g, temp.b, 0.15f);
         
         isTutorial = tutorial;
         
         text.text = subtitle;
+        Color.RGBToHSV(temp, out var H, out var S, out var V);
+        text.color = Color.HSVToRGB(H, S/2f, V);
+
         isPopulated = true;
     }
 
